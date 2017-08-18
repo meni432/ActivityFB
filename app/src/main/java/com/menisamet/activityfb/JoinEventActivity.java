7fe948f0b79b2241c7f958d63c81f0af599bd867
Meni Samet <meni432@gmail.com>
2017-08-18 07:37:29 +0300

    merge by hand event activity

--- app/src/main/java/com/menisamet/activityfb/JoinEventActivity.java
+++ app/src/main/java/com/menisamet/activityfb/JoinEventActivity.java
@@ -1,9 +1,20 @@
 package com.menisamet.activityfb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class JoinEventActivity extends AppCompatActivity {
+
+    private Button joinButton;
+    private TextView titleText;
+    private TextView titleName;
+    private TextView interestText;
+    private TextView interestName;
+    private TextView details;
 
 
     @Override
@@ -11,5 +22,28 @@ public class JoinEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_event);
 
+        joinButton = (Button) findViewById(R.id.joinButton);
+        titleText = (TextView) findViewById(R.id.title);
+        titleName = (TextView) findViewById(R.id.titleName);
+        interestText = (TextView) findViewById(R.id.interest);
+        interestName= (TextView) findViewById(R.id.interestName);
+        details = (TextView) findViewById(R.id.details);
+
+        titleText.setText("Title");
+        titleName.setText(getIntent().getStringExtra("TITLE_EVENT"));
+        interestText.setText("Interest");
+        interestName.setText(getIntent().getStringExtra("INTEREST_EVENT"));
+        joinButton.setText("Join");
+        details.setText(getIntent().getStringExtra("DETAILS_EVENT"));
+
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent myIntent = new Intent(getBaseContext(), haveFunActivity.class);
+//                myIntent.putExtra("USER_NAME",getIntent().getStringExtra("USER_EVENT"));
+//                startActivity(myIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/menisamet/activityfb/MainActivity.java
+++ app/src/main/java/com/menisamet/activityfb/MainActivity.java
@@ -64,4 +64,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    public void loadJoinEventActivity(View view) {
+        Intent intent = new Intent(this, JoinEventActivity.class);
+        startActivity(intent);
+    }
 }
